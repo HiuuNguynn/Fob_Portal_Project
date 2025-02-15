c3cc2be33d9c9c1b068974be496a054f4fd5d917
Hieu Nguyen <hieunguyenby04@gmail.com>
2025-02-15 13:12:47 +0700

    Resolved merge conflicts

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -17,7 +17,10 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->rememberToken();
+            $table->string('image')->nullable();
+            $table->string('designation')->nullable();
+            $table->string('mobile');
+            $table->rememberToken()->nullable();
             $table->timestamps();
         });
 
